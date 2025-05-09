a0078302e87240679cf433a7f201d05fafc08f06
ElyGodSot <elizabeth.godinez.s@hotmail.com>
2025-05-08 18:37:17 -0600

    se agrega uri

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import connectDB from './src/config/db.js'
-import { port } from './src/config/constants.js'
+import { port, uri } from './src/config/constants.js'
 
 connectDB();
 
@@ -8,6 +8,8 @@ const app = express();
 //nos permitira hacer uso de json(objetos) en peticiones
 app.use(express.json())
 
+app.use(uri,);
+
 // TODO HACER RUTA A MI ENDPOINT DE PRUEBA
 const PORT = port || 5000;
-app.listen(PORT, console.log(`Server runing on port ${PORT}`))
+app.listen(PORT, console.log(`Server runing on http://localhost:${PORT}${uri}`))
