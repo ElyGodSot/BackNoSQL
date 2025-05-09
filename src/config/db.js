290abdf47c65548878c6292a6044af8ef6a0c4cd
ElyGodSot <elizabeth.godinez.s@hotmail.com>
2025-05-08 18:10:31 -0600

    uso de constantes para buenas practicas

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,10 @@
 import mongoose from "mogoose"
+import {mongoDomain,mongoUser,mongoPwd,mongoDb} from './constants'
 const connectDB=async() =>{
     try{
-        await mongoose.connect()
-        console.log('moongoDB connected')
+        await mongoose.connect("mongodb+srv://elizabethgodinezs:nnuDAPUiabXKl7nk@godinez.roj9vab.mongodb.net/")
+        // await mongoose.connect(`${mongoDomain}${mongoUser}${mongoPwd};@${mongoDb}/`)
+        console.log('MongoDB connected')
     }catch(error){
         console.error(error)
         process.exit(1);
