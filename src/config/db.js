7b20b501b43c5849f5af4ccd29c0e95872a3ad52
ElyGodSot <elizabeth.godinez.s@hotmail.com>
2025-05-08 22:59:56 -0600

    ok

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose"
 import {mongoDomain,mongoUser,mongoPwd,mongoDb} from './constants.js'
-const connectDB=async() =>{
-    try{
-        await mongoose.connect(`${mongoDomain}${mongoUser}:${mongoPwd}@${mongoDb}/`)
-        console.log('MongoDB connected')
-    }catch(error){
-        console.error(error)
+const connectDB = async() => {
+    try {
+        await mongoose.connect(`${mongoDomain}${mongoUser}:${mongoPwd}@${mongoDb}`);
+        console.log('MongoDB Connected...');
+    } catch (error) {
+        console.error(error);
         process.exit(1);
     }
 }
-export default connectDB
+
+export default connectDB;
